36bef2407e971a8c43c6ea129c61b96657f8d658
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-01-05 23:17:52 +0100

    Add task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module for task 5"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Type annotated function to return the sum of a list of floats"""
+    return sum(input_list)
