32d5d14ebb7b763e04edfa8f1ebce098cfa2e795
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-01-09 19:40:39 +0100

    fix: fix requirement errors

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+"""
+0. The basics of async"""
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random delay between 0 and max_delay seconds and return it"""
